c6d5613b2880a9eff859b5445b73f7ee0cb157e6
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-04-09 11:31:36 +0300

    postprocessors/fusion360/stylecnc_lathe > dev: source

--- postprocessors/fusion360/stylecnc_lathe/source/07_onRapid.js
+++ postprocessors/fusion360/stylecnc_lathe/source/07_onRapid.js
@@ -1,9 +1,8 @@
 function onRapid(_x, _y, _z) {
     var x = xOutput.format(_x);
-    var y = yOutput.format(_y);
     var z = zOutput.format(_z);
 
-    if (x || y || z) {
-        writeBlock(gFormat.format(0), x, y, z);
+    if (x || z) {
+        writeBlock(gFormat.format(0), x, z);
     }
 }
--- postprocessors/fusion360/stylecnc_lathe/source/08_onLinear.js
+++ postprocessors/fusion360/stylecnc_lathe/source/08_onLinear.js
@@ -1,8 +1,7 @@
 function onLinear(_x, _y, _z, feed) {
     var x = xOutput.format(_x);
-    var y = yOutput.format(_y);
     var z = zOutput.format(_z);
     var f = getFeed(feed);
 
-    writeBlock(gFormat.format(1), x, y, z, f);
+    writeBlock(gFormat.format(1), x, z, f);
 }
